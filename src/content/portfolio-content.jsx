b25237c3cfc32a36d2520bfec1c5870d7b08eeab
mvdgp <mvandraanen@hotmail.com>
2025-03-25 16:33:41 +0100

    Chore: removed unnecessary content

--- src/content/portfolio-content.jsx
+++ src/content/portfolio-content.jsx
@@ -1,19 +1,5 @@
 export const portfolio = [
     {
-        name: 'Donnybap',
-        description: {
-            EN: 'A website for a pop-up restaurant in Amsterdam Noord, built with React.',
-            NL: 'Een website voor een pop-up restaurant in Amsterdam Noord, gebouwd met React.'
-        },
-        date:'2024',
-        image: {
-            large:'/Project-Donnybap-large.png',
-            small:'/Project-Donnybap-small.png'
-        },
-        url: 'https://www.donnybap.nl',
-        repo: 'https://github.com/mvdgp/donny.bap_2024'
-    },
-    {
         name: 'Fietsmaatjes',
         description: {
             EN: 'A website for a volunteers organization in Amsterdam Nieuw Sloten, built with NextJS, React and Prismic as headless CMS.',
@@ -26,5 +12,19 @@ export const portfolio = [
         },
         url: 'https://www.fietsmaatjesamsterdamnieuw-sloten.nl',
         repo: 'https://github.com/mvdgp/fietsmaatjes'
+    },
+    {
+        name: 'Donnybap',
+        description: {
+            EN: 'A website for a pop-up restaurant in Amsterdam Noord, built with React.',
+            NL: 'Een website voor een pop-up restaurant in Amsterdam Noord, gebouwd met React.'
+        },
+        date:'2024',
+        image: {
+            large:'/Project-Donnybap-large.png',
+            small:'/Project-Donnybap-small.png'
+        },
+        url: 'https://www.donnybap.nl',
+        repo: 'https://github.com/mvdgp/donny.bap_2024'
     },
 ];
